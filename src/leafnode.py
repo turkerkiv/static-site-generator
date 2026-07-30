6219ecd3446e675574e5c0ad0674a97ce989327a
Türker Kıvılcım <kivturker@gmail.com>
2026-07-30 12:22:39 +0300

    fix: allow imgs dont have value

--- src/leafnode.py
+++ src/leafnode.py
@@ -9,7 +9,7 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if not self.value and self.tag != "img":
             raise ValueError("All leaf nodes must have a value")
         if not self.tag:  # it is raw text
             return self.value
