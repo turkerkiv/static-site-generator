2a4a6790b12a7eec65b396aba22d7f5ef592f0c9
Türker Kıvılcım <kivturker@gmail.com>
2026-07-30 14:34:18 +0300

    fix: update main to accept cli arguments as base path for htmls

--- src/main.py
+++ src/main.py
@@ -2,23 +2,31 @@ from test_markdown import md
 from mdextractor import extract_title, whole_markdown_to_html_node
 import os
 import shutil
-
-project_path = os.path.abspath(".")
-public_dir_path = os.path.join(project_path, "public")
-static_dir_path = os.path.join(project_path, "static")
-content_dir_path = os.path.join(project_path, "content")
+import sys
 
 
 def main():
-    # reset public folder
-    if os.path.exists(public_dir_path):
-        shutil.rmtree(public_dir_path)
-    os.mkdir(public_dir_path)
+    base_path_for_html = "/"
+    if len(sys.argv) > 1 and sys.argv[1]:
+        base_path_for_html = f"{sys.argv[1]}"
 
-    copy_files_to_public(static_dir_path, public_dir_path)
+    project_root_path = os.path.abspath(".")
+    docs_dir_path = os.path.join(project_root_path, "docs")
+    static_dir_path = os.path.join(project_root_path, "static")
+    content_dir_path = os.path.join(project_root_path, "content")
+
+    # reset docs folder
+    if os.path.exists(docs_dir_path):
+        shutil.rmtree(docs_dir_path)
+    os.mkdir(docs_dir_path)
+
+    copy_files_to_public(static_dir_path, docs_dir_path)
 
     generate_all_pages_inside_dir(
-        content_dir_path, os.path.join(project_path, "template.html"), public_dir_path
+        content_dir_path,
+        os.path.join(project_root_path, "template.html"),
+        docs_dir_path,
+        base_path_for_html,
     )
 
 
@@ -34,19 +42,21 @@ def copy_files_to_public(from_dir_path, to_copy_path):
             copy_files_to_public(to_be_copied_path, new_folder_path)
 
 
-def generate_all_pages_inside_dir(path_to_search, template_path, dest_path):
+def generate_all_pages_inside_dir(path_to_search, template_path, dest_path, base_path):
     for path in os.listdir(path_to_search):
         from_path = os.path.join(path_to_search, path)
         new_dest_path = os.path.join(dest_path, path)
         if os.path.isfile(from_path):
             new_dest_path = new_dest_path.removesuffix(".md") + ".html"
-            generate_page(from_path, template_path, new_dest_path)
+            generate_page(from_path, template_path, new_dest_path, base_path)
         else:
             os.mkdir(new_dest_path)
-            generate_all_pages_inside_dir(from_path, template_path, new_dest_path)
+            generate_all_pages_inside_dir(
+                from_path, template_path, new_dest_path, base_path
+            )
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md = ""
     with open(from_path, "r") as file:
@@ -61,6 +71,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content_html_node.to_html())
+    template = template.replace('href="/', f'href="{base_path}')
+    template = template.replace('src="/', f'src="{base_path}')
 
     with open(dest_path, "w") as file:
         file.write(template)
