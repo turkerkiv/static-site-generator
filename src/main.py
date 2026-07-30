ad6ba8bf3f74057df7ac124340b2c9f25a7250fe
Türker Kıvılcım <kivturker@gmail.com>
2026-07-30 13:26:38 +0300

    add: implementation to generate all pages inside a dir

--- src/main.py
+++ src/main.py
@@ -17,23 +17,33 @@ def main():
 
     copy_files_to_public(static_dir_path, public_dir_path)
 
-    generate_page(
-        os.path.join(content_dir_path, "index.md"),
-        os.path.join(project_path, "template.html"),
-        os.path.join(public_dir_path, "index.html"),
+    generate_all_pages_inside_dir(
+        content_dir_path, os.path.join(project_path, "template.html"), public_dir_path
     )
 
 
 def copy_files_to_public(from_dir_path, to_copy_path):
     for path in os.listdir(from_dir_path):
         to_be_copied_path = os.path.join(from_dir_path, path)
-        print(to_be_copied_path)
+        print(f"copying: {to_be_copied_path}")
         if os.path.isfile(to_be_copied_path):
             shutil.copy(to_be_copied_path, to_copy_path)
         else:
             new_folder_path = os.path.join(to_copy_path, path)
             os.mkdir(new_folder_path)
             copy_files_to_public(to_be_copied_path, new_folder_path)
+
+
+def generate_all_pages_inside_dir(path_to_search, template_path, dest_path):
+    for path in os.listdir(path_to_search):
+        from_path = os.path.join(path_to_search, path)
+        new_dest_path = os.path.join(dest_path, path)
+        if os.path.isfile(from_path):
+            new_dest_path = new_dest_path.removesuffix(".md") + ".html"
+            generate_page(from_path, template_path, new_dest_path)
+        else:
+            os.mkdir(new_dest_path)
+            generate_all_pages_inside_dir(from_path, template_path, new_dest_path)
 
 
 def generate_page(from_path, template_path, dest_path):
