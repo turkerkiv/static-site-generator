49ee8b17589f09d2cde5987992449416000ce2d9
Türker Kıvılcım <kivturker@gmail.com>
2026-07-30 12:59:05 +0300

    add: create index.md and implement page generation from markdown to HTML

--- src/main.py
+++ src/main.py
@@ -1,18 +1,27 @@
 from test_markdown import md
-from mdextractor import markdown_to_html_root_node
+from mdextractor import extract_title, whole_markdown_to_html_node
 import os
 import shutil
 
 project_path = os.path.abspath(".")
 public_dir_path = os.path.join(project_path, "public")
 static_dir_path = os.path.join(project_path, "static")
+content_dir_path = os.path.join(project_path, "content")
 
 
 def main():
+    # reset public folder
     if os.path.exists(public_dir_path):
         shutil.rmtree(public_dir_path)
     os.mkdir(public_dir_path)
+
     copy_files_to_public(static_dir_path, public_dir_path)
+
+    generate_page(
+        os.path.join(content_dir_path, "index.md"),
+        os.path.join(project_path, "template.html"),
+        os.path.join(public_dir_path, "index.html"),
+    )
 
 
 def copy_files_to_public(from_dir_path, to_copy_path):
@@ -25,6 +34,26 @@ def copy_files_to_public(from_dir_path, to_copy_path):
             new_folder_path = os.path.join(to_copy_path, path)
             os.mkdir(new_folder_path)
             copy_files_to_public(to_be_copied_path, new_folder_path)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    md = ""
+    with open(from_path, "r") as file:
+        md = file.read()
+
+    template = ""
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    content_html_node = whole_markdown_to_html_node(md)
+    title = extract_title(md)
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", content_html_node.to_html())
+
+    with open(dest_path, "w") as file:
+        file.write(template)
 
 
 if __name__ == "__main__":
